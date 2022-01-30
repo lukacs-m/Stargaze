b8a5a109e110440484aad4a6d3c3f7be497a63bb
mlukacs <mlukacs@deezer.com>
2022-01-30 18:25:53 +0100

    Initial Commit

---
+++ Stargaze/StargazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StargazeApp.swift
+//  Stargaze
+//
+//  Created by Martin Lukacs on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct StargazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
