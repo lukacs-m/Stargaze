a4faba9f28349841237dc202411bbacac9c7ecfa
mlukacs <mlukacs@deezer.com>
2022-01-31 13:47:11 +0100

    Test of code cov

--- StargazeUITests/StargazeUITestsLaunchTests.swift
+++ StargazeUITests/StargazeUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
- class StargazeUITestsLaunchTests: XCTestCase {
+class StargazeUITestsLaunchTests: XCTestCase {
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
@@ -28,4 +28,4 @@ import XCTest
         attachment.lifetime = .keepAlways
         add(attachment)
     }
- }
+}
